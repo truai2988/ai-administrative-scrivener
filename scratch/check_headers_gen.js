860ec858e89efe3bb879657f047cb0a6f2d81e00
truai2988 <trueeye792@gmail.com>
2026-04-24 22:41:50 +0900

    22:41

--- scratch/check_headers_gen.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const t1 = fs.readFileSync('src/utils/renewalCsvGenerator.ts', 'utf8');
-
-const { renewalFormOptions } = require('./src/lib/constants/renewalFormOptions.js'); // Cannot require ts directly
--- test_templates.ts
+++
@@ -1,6 +0,0 @@
-import { getAssignmentTemplates } from './src/lib/constants/assignmentTemplates'; 
-async function main() { 
-  const tmp = await getAssignmentTemplates(); 
-  console.log(JSON.stringify(tmp, null, 2)); 
-} 
-main();
